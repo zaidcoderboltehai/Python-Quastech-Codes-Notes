e5e492992b03d502eac11f67d6c6ad751c5598a0
zaidcoderboltehai <157005476+zaidcoderboltehai@users.noreply.github.com>
2024-04-17 13:25:10 +0530

    17 april 2024

--- ormproject/firstapp/urls.py
+++ ormproject/firstapp/urls.py
@@ -3,5 +3,9 @@ from . import views as v
 urlpatterns = [
     path("",v.home),
     path("add-user",v.addUser),
-    path("list",v.list)
+    path("list",v.list),
+    path("delete",v.delete),
+    path("delete2/<int:pk>",v.delete2),
+    path("edit/<int:pk>",v.edit),
+    path("search",v.search)
 ]
--- ormproject/firstapp/views.py
+++ ormproject/firstapp/views.py
@@ -26,3 +26,36 @@ def addUser(request):
 def list(request):
     users=User.objects.all()
     return render(request,"list.html",{"users":users})
+
+def delete(request):
+    id=request.GET.get("id")
+    user=User.objects.get(id=id)
+    user.delete()
+    return redirect("/list")
+
+def delete2(request,pk):
+    user=User.objects.get(id=pk)
+    user.delete()
+    return redirect("/list")
+
+def edit(request,pk):
+    user=User.objects.get(id=pk)
+    if(request.method=="POST"):
+        name=request.POST.get("name")
+        phone=request.POST.get("phone")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        user.name=name
+        user.phone=phone
+        user.email=email
+        user.password=password
+        user.save()
+        return redirect("/list")
+    else:
+        return render(request,"editform.html",{"user":user})
+
+def search(request):
+    srch=request.GET.get("srch")
+    users=User.objects.filter(name__contains=srch)
+    return render(request,"list.html",{"users":users})
+
